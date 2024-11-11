53c276bc8f5822def6780420446d7d4d4909fd70
pranav-nani <pranavreddy1412@gmail.com>
2024-11-11 17:37:37 +0530

    edit_5

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
