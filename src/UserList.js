5b109f3a4cafabf0e67db76b360b4bdfadf2c16a
pranav-nani <pranavreddy1412@gmail.com>
2024-11-11 16:48:20 +0530

    edit

--- src/UserList.js
+++ src/UserList.js
@@ -4,7 +4,7 @@ const UserList = ({ users, onEdit, onDelete }) => (
     <div>
         <h2>Registered Users</h2>
         <ul>
-            {users.map((user) => (
+            {users && users.map((user) => (
                 <li key={user.id}>
                     {user.name} ({user.email}, {user.dob})
                     <button onClick={() => onEdit(user)}>Edit</button>
